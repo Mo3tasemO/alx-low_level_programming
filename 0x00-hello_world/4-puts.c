570a8b1bf5dbb7e5ff49674b08a11abf6ba6c67b
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-11 17:20:10 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-        int main() {
-        puts("\"Programming is like building a multilingual puzzle\n");
-
-
-        return 0;
-
-        }
+gcc puts.c | ./a.out
