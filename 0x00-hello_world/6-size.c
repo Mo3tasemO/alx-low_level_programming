a2f3fe7191cb2abab47d482a3ba0f23e8b4c8915
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-11 08:30:14 -0700

    some

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+	int main(){
+	printf("Size of a char:%ld bytes\n",sizeof(char));
+	printf("Size of a int:%ld bytes\n",sizeof(int));
+	printf("Size of a long int:%ld bytes\n",sizeof(long int));
+	printf("Size of a long long int:%ld bytes\n",sizeof(long long int));
+	printf("Size of a float:%ld bytes\n",sizeof(float));
+
+	return 0;
+	
+	
+	
+	}
