7976601a066a9ee42d83447ef10af95eacf0e8ca
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-13 08:35:32 -0700

    some

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - print numbers
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 48;
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
