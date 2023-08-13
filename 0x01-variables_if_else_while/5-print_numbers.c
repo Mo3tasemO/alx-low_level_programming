8481132190cc419dc4274131bf9a8b1d85605462
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-13 08:21:02 -0700

    some

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - print numbers
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+while (i <= 9)
+{
+printf("%d", i);
+i++;
+}
+printf("\n");
+return (0);
+}
