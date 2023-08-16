1745cef5e074551ecf6d93efc68dae0f3cf35ca1
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-16 23:57:50 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
- * main - functions and nestedloops
- *
- * Return: Alway 0 (Sucess)
- */
-
+* main - print _putchar
+* is written by mbah
+* Return: 0
+*/
 int main(void)
-int _putchar(char c)
 {
-	char str[] = "_putchar";
-	int ch;
-	for (ch = 0; ch <= 8; ch++)
-	         _putchar(str[ch]);
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
