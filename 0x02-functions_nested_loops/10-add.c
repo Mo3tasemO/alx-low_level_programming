6e1fcc18fcd5a59583031953dc9a5e08f299a1aa
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-17 15:33:08 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,9 +3,9 @@
  * add -  adds two integers and returns the result
  * Return: Always 0 (Success)
  */
-int add(int, int)
+int add(int a, int b)
 {
-int n = add(89, 9);
+int n = add(a, b);
 _putchar('n');
 _putchar('\n');
 return (0);
