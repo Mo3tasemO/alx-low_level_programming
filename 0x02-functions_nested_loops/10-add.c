1aafede14894113e2203aaf22f07a6a71fba217d
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-17 15:41:13 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,13 @@
 #include"main.h"
 /**
  * add -  adds two integers and returns the result
- * Return: Always 0 (Success)
+ * @a: int to be added to b
+ * @b: int to be added to a
+ *Return: Always 0 (Success)
  */
 int add(int a, int b)
 {
-int n = add(a, b);
-n = 0;
-while (n >= 0)
-{
-_putchar('n');
-}
-_putchar('\n');
-return (n);
+int c;
+c = a + b
+return (c);
 }
