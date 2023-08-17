18fd0e3e9771b09a7a8db44084e9696a6c431f5b
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-17 05:31:15 -0700

    s

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * add -  adds two integers and returns the result
+ * Return: Always 0 (Success)
+ */
+int add(int, int)
+{
+int n = add(89, 9);
+_putchar('n');
+_putchar('\n');
+return (0);
+}
