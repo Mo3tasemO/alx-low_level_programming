930b2ea79c602cbd14ac262b78f1c993f3d08af7
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-17 14:35:03 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * _abs - computes the absolute value of an integer
+ * @n: is the char to be checked
  * Return: 1 and prints + if n is greater than zero
  */
 int _abs(int n)
 {
-if (n > 0)
+if (n >= 0)
 {
 return (n);
 }
