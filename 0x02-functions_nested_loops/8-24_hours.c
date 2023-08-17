86df9f0327ecdcc1439ad6508a281ba9234e0119
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-17 05:03:38 -0700

    s

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * @c: is the char to be checked
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+char c;
+for (c = '0'0:0'0'; c <= '23:59'; c++)
+{
+_putchar('c');
+}
+_putchar('\n');
+return (0);
+}
