2a002c4d438df8703dbc12e9aaeb2eefad978381
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-17 05:49:22 -0700

    s

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: is the char to be checked
+ *Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+for (n = '0'; n <= 98; n++)
+{
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+return (0);
+}
