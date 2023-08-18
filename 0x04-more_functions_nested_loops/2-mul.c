b7fb78dbef0ec52c3c0020d5f5094e547dc37aa8
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-18 16:01:30 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 #include"main.h"
 /**
-* mul -  multiplies two integers
+* mul - multiplies two integers
 * @a: the int for the paramaters of my function
-* @b: the int for the paramaters of my function 
+* @b: the int for the paramaters of my function
 * Return: Always 0 (Success)
 */
 int mul(int a, int b)
