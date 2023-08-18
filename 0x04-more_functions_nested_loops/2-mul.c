24e097c59a77ee4185cc9b2eef9d513ce637894c
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-18 16:00:21 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,11 @@
 #include"main.h"
 /**
 * mul -  multiplies two integers
-* @a: int to be multiply to b
-* @b: int to be multiply to a 
+* @a: the int for the paramaters of my function
+* @b: the int for the paramaters of my function 
 * Return: Always 0 (Success)
 */
 int mul(int a, int b)
 {
-int c;
-c = a * b
-_putchar('c');
-return (o);
+return (a * b);
 }
