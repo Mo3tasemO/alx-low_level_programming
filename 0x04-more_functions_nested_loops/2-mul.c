fd76c5b5362ee58056550010fdcb3031fc8c428c
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-18 05:53:55 -0700

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+* mul -  multiplies two integers
+* 
+* Return: Always 0 (Success)
+*/
+int mul(int a, int b)
+{
+int c;
+c = a * b
+return (c);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers -  prints the numbers, from 0 to 9, followed by a new line.
+ * @c: is the char to be checked
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+_putchar('c');
+}
+_putchar('\n');
+return (0);
+}
