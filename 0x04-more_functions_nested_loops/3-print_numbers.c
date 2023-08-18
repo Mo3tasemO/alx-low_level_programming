10e5b79a4833f2b2b45f69fed2b122914a3316a9
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-18 16:07:07 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers -  prints the numbers, from 0 to 9, followed by a new line.
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
  * @c: is the char to be checked
  * Return: Always 0.
  */
