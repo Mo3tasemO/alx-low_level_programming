2af6337dbcb4366df96b07959088ce242a69ca4b
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-18 12:46:24 -0700

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: is the integer for the paramaters of my function
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+a = 0;
+if (n > 0)
+{
+while (a <= n)
+{
+_putchar('_');
+a++;
+}
+_putchar('\n');
+}
+else
+_putchar('\n');
+}
