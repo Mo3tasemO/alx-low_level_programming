40e6ddf0653a7771959405773a47c62260962fc1
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-21 09:42:47 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,21 @@
 void print_diagonal(int n)
 {
 int a, b;
-if (n <= 0)
-_putchar('\n');
-else
-{
-for (a = 1; a <= n; a++)
+for (a = 0; a <= n; a++)
 {
 for (b = 1; b <= a; b++)
 _putchar(' ');
 _putchar(92);
 _putchar('\n');
 }
+if (n <= 0)
+_putchar('\n');
+else
+for (a = 1; a <= n; a++)
+{
+for (b = 1; b <= a; b++)
+_putchar(' ');
+_putchar(92);
+_putchar('\n');
 }
 }
