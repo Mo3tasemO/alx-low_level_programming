712b515a8288b81f0d0e28007900d271f016b086
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-23 02:50:06 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * reset_to_98 - apointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - parameter and updates the value it points to to 98
+ * @n: to be checked
  * Return: Always 0.
  */
 void reset_to_98(int *n)
