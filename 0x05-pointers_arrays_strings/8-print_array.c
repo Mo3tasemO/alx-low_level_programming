c9e42970841fa8adf304e3ddab25cb697252e542
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-23 08:38:05 -0700

    a

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * print_array - prints n elements of an array of integers, followed by a new line
+ * @a: to be checked
+ * @n: to be checked
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+printf("%d; ", a[0]);
+printf("%d; ", a[1]);
+printf("%d; ", a[2]);
+printf("%d; ", a[3]);
+printf("%d", a[4]);
+}
