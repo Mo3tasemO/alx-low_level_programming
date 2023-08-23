71a929cf39816b981434f596424ab104bf860f5c
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-23 16:56:36 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,13 @@
  */
 void puts_half(char *str)
 {
-int n = strlen(str);
-int len = n / 2;
-int len1 = n - len;
-for (len1 = 0; str[len1] != '\0'; len1++)
+int i = strlen(str);
+int len = i / 2;
+int len1 = i - len;
+int n = len1 - 1;
+for (n = 0; str[n] != '\0'; n++)
 {
-_putchar(str[len1]);
+_putchar(str[n]);
 }
 _putchar('\n');
 }
