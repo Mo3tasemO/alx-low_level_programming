91d02774e383d70586204d17f2164d2b7b57477f
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-23 09:18:11 -0700

    a

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: to be checked
+ * @src: to be checked
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *p = dest + '\0';
+strcpy(p, src);
+printf(dest);
+}
