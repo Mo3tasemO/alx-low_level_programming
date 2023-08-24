84054f761fac81ab87751e5f08f03f4b68c9dce8
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-25 00:47:34 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,6 +4,7 @@
  * _strncat -  function is similar to the _strcat function
  * @dest: to be checked
  * @src: to be checked
+ * @n: to be checked
  * Return: Always 0.
  */
 char *_strncat(char *dest, char *src, int n)
@@ -14,11 +15,10 @@ while (dest[len] != '\0')
 {
 len++;
 }
-for (i = 0; src[i] != '\0'; i++, len++)
+for (i = 0; src[j] != '\0' && j < n; i++, len++)
 {
 dest[len] = src[i];
 }
 dest[len] = '\0';
-puts(dest);
 return (dest);
 }
