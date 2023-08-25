64ad2871e1bc3deb823c77349c4a469e6780c444
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-25 18:59:34 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,7 +2,6 @@
 /**
  * leet - encodes a string in 1337
  * @s: string to be encoded
- *
  * Return: the resulting string;
  */
 char *leet(char *s)
@@ -12,10 +11,12 @@ char *a = "aAeEoOtTlL";
 char *b = "4433007711";
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 10, j++)
+for (j = 0; j < 10; j++)
 {
-if (s[i] = a[j])
+if (s[i] == a[j])
+{
 s[i] = b[j];
+}
 }
 }
 return (s);
