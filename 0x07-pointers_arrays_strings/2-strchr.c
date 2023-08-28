636cce8fa7c8fccfdcc7fbf48e3e667b8bc7f809
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-28 10:41:19 -0700

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include <string.h>
 /**
  * _strchr - locates a character in a string.
  * @s: to be checked
@@ -8,7 +9,8 @@
 char *_strchr(char *s, char c)
 {
 int i;
-for (i = 0; i < strlen(c); i++)
+char alpha[] = "c";
+for (i = 0; i < strlen(alpha); i++)
 {
 if (s[i] = c)
 {
