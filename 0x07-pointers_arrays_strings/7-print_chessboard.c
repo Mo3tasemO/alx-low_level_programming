1914c1e0536b8c65e156270914e255ff7c977807
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-30 09:03:03 -0700

    a

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,11 +6,18 @@
  */
 void print_chessboard(char (*a)[8])
 {
-int i;
-for (i = 0; a[i] != '\0'; i++)
+int i, j;
+for (i = 0; i < 8; i++)
 {
-if (a[i] == 2 && a[i] == 3 && a[i] == 4 && a[i] == 5 && a[i] == 6)
-_putchar(' ');
-continue;
+for (j = 0; j < 8; j++)
+{
+if (j == 7)
+{
+_putchar(a[i][j]);
+_putchar('\n');
+}
+else
+_putchar(a[i][j]);
+}
 }
 }
