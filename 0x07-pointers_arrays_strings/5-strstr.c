4bcd6be86a891a59a8a7c40c4e07d5b09381de9d
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-08-30 08:10:38 -0700

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strstr - locates a substring
+ * @haystack: to be checked
+ * @needle: to be checked
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+while (*needle)
+{
+for (i = 0; haystack[i]; i++)
+{
+if (*needle == haystack[i])
+return (needle);
+else 
+return (haystack);
+}
+needle++;
+}
+return (NULL);
+}
