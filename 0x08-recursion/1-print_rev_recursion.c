2ecc7dcfe9816290f20ac7e9eef3e85be139a639
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-31 19:17:12 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- * _print_rev_recursion - prints a string in reverse. 
+ * _print_rev_recursion - prints a string in reverse.
  * @s: to be checked
  * Return: Always 0.
  */
@@ -11,6 +11,6 @@ if (*s == '\0')
 _putchar('\n');
 return;
 }
-_putchar(*s);
+_putchar(*s - 1);
 _print_rev_recursion(s - 1);
 }
