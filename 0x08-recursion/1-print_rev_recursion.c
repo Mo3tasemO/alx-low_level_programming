3e469cc0159bc8ef114aafffcc6637ccd78fabc0
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-08-31 19:29:26 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,12 +8,7 @@
  */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar('\n');
-return;
-}
-char k = *s - 1;
-_putchar(k);
+if (*s != '\0')
+_putchar(*s);
 _print_rev_recursion(s + 1);
 }
