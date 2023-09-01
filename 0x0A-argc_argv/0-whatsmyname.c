e3645230a1d6ec5a684ca1a710aaf3199525a621
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-09-01 14:32:00 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * @argc: to be checked
+ * @argv: to be checked
+ * Return: Succes (0)
+*/
+int main (int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * @argc: to be checked
+ * @argv: to be checked
+ * Return: Succes (0)
+*/
+int main (int argc, char *argv[])
+{
+int c;
+(void)argc;
+for (c = 0; c <= argc; c++)
+{}
+printf("%d\n", c - 2);
+return (0);
+}
