5a87dabfaa871be8c6331aeecaf11853be6def0d
Mo3tasemO <MoatasemElsharkawy12@gmail.com>
2023-09-06 18:45:15 +0300

    a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include "main.h"
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @c: to be checked
+ * @size: to be checked
+ * Return: Success 0
+ */
+char *create_array(unsigned int size, char c)
+{
+unsigned int i = 0;
+char *str;
+str = malloc (size * sizeof(char));
+if (size == 0 || str == NULL)
+{
+return (NULL);
+}
+while (i < size)
+{
+str[i] = c;
+i++;
+}
+free(str);
+return (str);
+}
