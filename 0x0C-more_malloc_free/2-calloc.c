9dc8196558d40edc0d95ba773598c7627d131656
Mo3tasemO <137957102+Mo3tasemO@users.noreply.github.com>
2023-09-08 01:01:14 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,8 +5,8 @@
  * _calloc - allocates memory for an array
  * @nmemb: to be checked
  * @size: the size of the memory to print
- * Return: Nothing.
- */
+ * Return: Success 0
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 int i = 0, l = 0;
@@ -14,10 +14,10 @@ char *p;
 if (nmemb == 0 || size == 0)
 return (NULL);
 l = nmemb * size;
-p = malloc(1);
+p = malloc(l);
 if (p == NULL)
 return (NULL);
-while (i < 1)
+while (i < l)
 {
 p[i] = 0;
 i++;
